4e55b831cd7839d6440f21e47f70b0148f832ff4
Tomasz Szarek <tomasz.szarek@sollers.eu>
2018-05-17 10:14:34 +0200

    Odejście z bootstrap part 2

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import MainPage from './js/Components/MainPage/MainPage'
 import { Provider } from 'react-redux'
 import reducer from './js/reducers'
 import { createStore, applyMiddleware } from 'redux'
-import { loadTasks } from './js/actions'
 import thunk from 'redux-thunk'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 ReactDOM.render(
